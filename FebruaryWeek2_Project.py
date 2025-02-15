57b10f8f6d3ab16349e440441981e55a2e388d91
Derek Kweku Degbedzui <47590063+dkdegbedzui@users.noreply.github.com>
2025-02-15 15:27:16 -0500

    February Week 2 - Weekend Hands-on Project

---
+++ FebruaryWeek2_Project.py
@@ -0,0 +1,7 @@
+## Weekly Weekend Hands-on Project
+# Hands-on Projects with EDA, AB Testing & Business Intelligence
+# https://youtu.be/FTpmwX94_Yo?list=PLrpC1AhCG6Yd4BIC-HerhHUvPo1PC9Os8
+
+## AB Testing https://github.com/TatevKaren/CaseStudies/tree/main/AB%20Testing
+
+## Super Store Data Analysis https://github.com/TatevKaren/CaseStudies/tree/main/SuperStoreCaseStudy
