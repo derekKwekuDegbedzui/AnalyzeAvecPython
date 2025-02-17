5be7f7a091b0da794d5175ebb8225a6716d16295
Derek Kweku Degbedzui <47590063+dkdegbedzui@users.noreply.github.com>
2025-02-17 16:27:42 -0500

    Advanced Python - empty file

---
+++ FebruaryWeek3_AdvancedPython.py
@@ -0,0 +1,8 @@
+## February Week 3: Advanced Python
+# https://youtube.com/playlist?list=PL30AETbxgR-eczfB1DsAyr4L3_oyDIWAA&si=5rycPWkkK2rT4Cst
+
+# Alt + Shift + E : run highlighted line
+# Ctrl + Shift + F10: run current file
+# Shift + F6: refactor variable name
+# Ctrl + K: Commit
+
